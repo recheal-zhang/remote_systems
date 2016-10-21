e4b0a377741aecf22475216903c764739bb12fae
intern_zhanglichao <intern_zhanglichao@cvte.com>
2016-10-20 17:52:08 -0700

    delete src

--- client/src/testexe.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <unistd.h>
-
-int main(){
-    //execl("/usr/bin/gnome-screenshot", "gnome-screenshot", "-f", "testp" , NULL);
-    execl("/bin/cat", "/bin/cat", "temp", NULL);
-    return 0;
-}
-
